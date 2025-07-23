6039e5512f030bba48cb6ff95b6cd2927352fa05
Berat-Ayvaz <118663438+Berat-Ayvaz@users.noreply.github.com>
2025-07-23 23:23:33 +0300

    added fonts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,13 +18,13 @@ const Navbar = () => {
                 />
             </a>
             <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
-                <li><a href="#top">Ana Sayfa</a></li>
-                <li><a href="#aboutme">Hakkımda</a></li>
-                <li><a href="#services">Servisler</a></li>
-                <li><a href="#mywork">İşlerim</a></li>
+                <li><a className='' href="#top">Ana Sayfa</a></li>
+                <li><a className='font-Ovo' href="#aboutme">Hakkımda</a></li>
+                <li><a className='font-Ovo' href="#services">Servisler</a></li>
+                <li><a className='font-Ovo' href="#mywork">İşlerim</a></li>
             </ul>
             <div className="">
-                <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4">
+                <a href="#contact" className=" hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4">
                     İletişim
                     <Image src={arrowIcon} alt="İletişim ikonu" width={12} height={12} className="w-3" />
                 </a>
