d38c87fa203e944592d11d33d381615c726f2bec
Berat-Ayvaz <118663438+Berat-Ayvaz@users.noreply.github.com>
2025-07-28 19:53:30 +0300

    Scroll added to Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import logo from '@/public/assets/logo (2).png';
 import headercolor from '@/public/assets/header-bg-color.png';
@@ -8,6 +8,7 @@ import moonIcon from '@/public/assets/moon_icon.png';
 import menu_black from '@/public/assets/menu-black.png';
 import close_black from '@/public/assets/close-black.png';
 const Navbar = () => {
+    const [IsScroll, setIsScroll] = React.useState(false);
     const sideMenuRef = useRef<HTMLUListElement>(null);
     const openMenu = () => {
         if (sideMenuRef.current) {
@@ -18,17 +19,26 @@ const Navbar = () => {
         if (sideMenuRef.current) {
             sideMenuRef.current.style.transform = 'translateX(16rem)';
         }
-    };
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (scrollY > 50) {
+                setIsScroll(true);
+            } else {
+                setIsScroll(false);
+            }
+        })
+    }, []);
     return (
         <>
             <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'> <Image
                 src={headercolor}
-                alt='Header Background Color'
+                alt='Header background color'
                 className='w-full'
             />
             </div>
-            <nav className='flex items-center justify-between py-4 w-full fixed px-5 lg:px-8 xl:[8%]
-        z-50'>
+            <nav className={`flex items-center justify-between py-4 w-full fixed px-5 lg:px-8 xl:[8%]
+        z-50 ${IsScroll ? "bg-white/75 backdrop-blur-lg shadow-sm" : ""}`}>
                 <a href="#top">
                     <Image
                         src={logo}
@@ -38,20 +48,21 @@ const Navbar = () => {
                         className="cursor-pointer mr-14"
                     />
                 </a>
-                <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+                <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${IsScroll ? "" : "bg-white/50 shadow-sm"} `}>
                     <li><a className='font-ovo' href="#top">Home</a></li>
                     <li><a className='font-ovo' href="#aboutme">About me</a></li>
                     <li><a className='font-ovo' href="#services">Services</a></li>
                     <li><a className='font-ovo' href="#mywork">My work</a></li>
+                    <li><a className='font-ovo' href="#contactme">Contact me</a></li>
                 </ul>
                 <div className="flex items-center gap-4">
                     <button className=''><Image
                         src={moonIcon}
                         alt='moon icon'
-                        className='w-8 h-8 hidden lg:block mr-5 rounded-full p-1 border-2 border-gray-500 hover:bg-gray-200 transition duration-500 cursor-pointer'
+                        className='w-8 h-8 hidden lg:block mr-5 rounded-full p-1 hover:bg-gray-200 transition duration-500 cursor-pointer'
                     /></button>
                     <a href="#contact" className=" hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo  hover:bg-gray-200 transition duration-500">
-                        İletişim
+                        Contact
                         <Image src={arrowIcon} alt="İletişim ikonu" className="w-3" />
                     </a>
                     <button className='block md:hidden ml-3' onClick={openMenu}><Image
@@ -60,7 +71,6 @@ const Navbar = () => {
                         className='w-6 cursor-pointer pb-10'
                     /></button>
                 </div>
-
 
                 {/* ----------------Mobile Menu----------------- */}
                 <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 
@@ -77,12 +87,10 @@ const Navbar = () => {
                     <li><a className='font-ovo' onClick={closeMenu} href="#aboutme">About me</a></li>
                     <li><a className='font-ovo' onClick={closeMenu} href="#services">Services</a></li>
                     <li><a className='font-ovo' onClick={closeMenu} href="#mywork">My work</a></li>
+                    <li><a className='font-ovo' onClick={closeMenu} href="#contactme">Contact me</a></li>
                 </ul>
             </nav>
-
         </>
-
     );
 };
-
 export default Navbar;
